168be770f2e0115cc90f55aaf05273ee66c15a9f
Pieter <pieterpaulussen@code-source.be>
2020-12-03 16:20:53 +0100

    [FIX] do not reset product packaging when no values are provided

--- addons/sale_stock/models/sale_order.py
+++ addons/sale_stock/models/sale_order.py
@@ -275,7 +275,8 @@ class SaleOrderLine(models.Model):
     @api.onchange('product_uom_qty', 'product_uom', 'route_id')
     def _onchange_product_id_check_availability(self):
         if not self.product_id or not self.product_uom_qty or not self.product_uom:
-            self.product_packaging = False
+            # Deactivated False setting for DAPHA-848
+            # self.product_packaging = False
             return {}
         if self.product_id.type == 'product':
             precision = self.env['decimal.precision'].precision_get('Product Unit of Measure')
